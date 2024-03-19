881cac28d4c520a6b238cc591651f14e9087328b
Taras Shcherban <147646967+Miki535@users.noreply.github.com>
2024-03-19 19:55:07 +0200

    Update main.go
    Add comments

--- main.go
+++ main.go
@@ -16,13 +16,16 @@ func HomePage(w http.ResponseWriter, r *http.Request) {
 
 func GenerateQRCode(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "POST" {
+		// parsing url from html
 		url := r.FormValue("url")
+		// error if url nil
 		if url == "" {
 			http.Error(w, "Please enter information", http.StatusBadRequest)
 			return
 		}
-
+		// generate qr code
 		qr, err := qrcode.Encode(url, qrcode.Medium, 256)
+		// error while generating qrcode
 		if err != nil {
 			http.Error(w, "Error generating QR code", http.StatusInternalServerError)
 			return
